be1eec8531d6682242c393364cdee3ad96111558
fionaly89 <liyue271181776@gmail.com>
2020-10-13 15:20:47 +0800

    发布1.1.25,修改BUG

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.1.24,修改BUG"
+git commit -a -m "发布1.1.25,修改BUG"
 git push origin master
-git tag 1.1.24  
+git tag 1.1.25  
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
