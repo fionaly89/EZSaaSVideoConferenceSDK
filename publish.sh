e6b9faca18e283c4165930a91bdbb33bb3c078a6
fionaly89 <liyue271181776@gmail.com>
2020-11-05 10:57:02 +0800

    发布1.1.29,新增加载过程

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.1.28,修改BUG"
+git commit -a -m "发布1.1.29,新增加载过程"
 git push origin master
-git tag 1.1.28  
+git tag 1.1.29  
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
