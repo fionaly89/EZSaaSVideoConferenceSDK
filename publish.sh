486fea38f455bf7b178f1cbcd11dd518f3db96f4
fionaly89 <liyue271181776@gmail.com>
2020-11-07 11:23:43 +0800

    发布1.1.30,修改音频采样率默认值

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.1.29,新增加载过程"
+git commit -a -m "发布1.1.30,修改音频采样率默认值"
 git push origin master
-git tag 1.1.29  
+git tag 1.1.30  
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
