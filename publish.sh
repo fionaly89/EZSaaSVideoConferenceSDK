2ef436eff55c32128335f6686569fda20d15c8fa
fionaly89 <liyue271181776@gmail.com>
2020-11-17 19:29:40 +0800

    发布1.1.33,修改被退出会议处理逻辑

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.1.32,修改被退出会议处理逻辑"
+git commit -a -m "发布1.1.33,修改被退出会议处理逻辑"
 git push origin master
-git tag 1.1.32  
+git tag 1.1.33  
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
