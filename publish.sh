55e29901cbd6bbb992a9836043ebfb545afb9ba6
fionaly89 <liyue271181776@gmail.com>
2020-12-09 19:46:40 +0800

    发布1.1.35,修改多语言逻辑

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.1.34,修改呼叫展示逻辑"
+git commit -a -m "发布1.1.35,修改多语言逻辑"
 git push origin master
-git tag 1.1.34  
+git tag 1.1.35  
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
