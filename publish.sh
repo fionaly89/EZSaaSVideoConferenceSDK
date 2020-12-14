9a1ea54a7aa94b94310ae604c0a01cc1ee3abfdf
fionaly89 <liyue271181776@gmail.com>
2020-12-14 13:55:08 +0800

    发布1.1.38,修改主持人状态判断逻辑

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.1.37,修改多语言逻辑"
+git commit -a -m "发布1.1.38,修改主持人状态判断逻辑"
 git push origin master
-git tag 1.1.37  
+git tag 1.1.38  
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
