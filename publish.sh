0b83d6d57b495512ad820121e280c89ad24f4fe2
fionaly89 <liyue271181776@gmail.com>
2020-12-14 19:35:15 +0800

    发布1.1.39,新增日志输出

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.1.38,修改主持人状态判断逻辑"
+git commit -a -m "发布1.1.39,新增日志输出"
 git push origin master
-git tag 1.1.38  
+git tag 1.1.39  
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
