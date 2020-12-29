6fb180664fd0621152c7960e67189f69c3f14667
fionaly89 <liyue271181776@gmail.com>
2020-12-29 18:17:57 +0800

    发布1.1.41,兼容新SDK

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.1.40,兼容新SDK"
+git commit -a -m "发布1.1.41,兼容新SDK"
 git push origin master
-git tag 1.1.40  
+git tag 1.1.41  
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
