3d1b9325dc45a249df8436f471f3bda2876c4cc5
fionaly89 <liyue271181776@gmail.com>
2020-12-29 19:41:19 +0800

    发布1.1.42,调整颜色

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.1.41,兼容新SDK"
+git commit -a -m "发布1.1.42,调整颜色"
 git push origin master
-git tag 1.1.41  
+git tag 1.1.42  
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
