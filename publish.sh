c49c6384361c3cedbbc769ed5aa348b05dbd3691
fionaly89 <liyue271181776@gmail.com>
2020-12-29 20:14:46 +0800

    发布1.1.43,修改图片获取逻辑

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.1.42,调整颜色"
+git commit -a -m "发布1.1.43,修改图片获取逻辑"
 git push origin master
-git tag 1.1.42  
+git tag 1.1.43  
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
