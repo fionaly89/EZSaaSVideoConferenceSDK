da0c6882a455d63ac71560c02975494a1f22024d
fionaly89 <liyue271181776@gmail.com>
2021-01-18 20:54:16 +0800

    发布1.1.49,数据转换保护

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.1.48,修改被叫页面逻辑错误问题"
+git commit -a -m "发布1.1.49,数据转换保护"
 git push origin master
-git tag 1.1.48  
+git tag 1.1.49  
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
