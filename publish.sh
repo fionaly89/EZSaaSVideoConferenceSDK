700c248141c50eb2eed3acc2d92dca2c37f48798
fionaly89 <liyue271181776@gmail.com>
2021-01-18 10:04:54 +0800

    发布1.1.47,修改被叫页面逻辑错误问题

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.1.46,修改被叫页面逻辑错误问题"
+git commit -a -m "发布1.1.47,修改被叫页面逻辑错误问题"
 git push origin master
-git tag 1.1.46  
+git tag 1.1.47  
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
