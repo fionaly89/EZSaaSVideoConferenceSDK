9762a8a888855ff2a3e663b4a3774d1558fa32f7
fionaly89 <liyue271181776@gmail.com>
2021-02-01 16:52:45 +0800

    发布1.1.51,修改内测问题

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.1.45,新增单独会议逻辑"
+git commit -a -m "发布1.1.51,修改内测问题"
 git push origin master
-git tag 1.1.45  
+git tag 1.1.51  
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
