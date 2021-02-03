81908765c56ea813ee02b7c3c60287312bf5e800
fionaly89 <liyue271181776@gmail.com>
2021-02-03 10:10:46 +0800

    发布1.1.52,修改内测问题

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.1.51,修改内测问题"
+git commit -a -m "发布1.1.52,修改内测问题"
 git push origin master
-git tag 1.1.51  
+git tag 1.1.52  
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
