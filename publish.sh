2bd96c8e30a5487d70244aad51313642e4647be3
fionaly89 <liyue271181776@gmail.com>
2021-02-05 11:12:41 +0800

    发布1.1.56,新增外部依赖

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.1.55,修改内测问题"
+git commit -a -m "发布1.1.56,新增外部依赖"
 git push origin master
-git tag 1.1.55  
+git tag 1.1.56  
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
