411a533cc0050e733d9ee8c2b99a7c8f985f2a8c
fionaly89 <liyue271181776@gmail.com>
2021-02-07 20:03:08 +0800

    发布1.1.59,解决小窗口问题

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.1.58,新增外部依赖"
+git commit -a -m "发布1.1.59,解决小窗口问题"
 git push origin master
-git tag 1.1.58  
+git tag 1.1.59  
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
