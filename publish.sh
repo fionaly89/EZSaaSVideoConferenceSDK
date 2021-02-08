247a9a5ca9c47ec0f33a700ba3af2027a7c940b9
fionaly89 <liyue271181776@gmail.com>
2021-02-08 15:04:28 +0800

    发布1.1.60,规避摄像头关闭时切换前后摄像头导致的崩溃问题

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.1.59,解决小窗口问题"
+git commit -a -m "发布1.1.60,规避摄像头关闭时切换前后摄像头导致的崩溃问题"
 git push origin master
-git tag 1.1.59  
+git tag 1.1.60  
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
