748784415ab6acd93c9ee1f607311a55ad59b758
fionaly89 <liyue271181776@gmail.com>
2021-03-17 15:44:48 +0800

    发布1.1.62,修改呼叫逻辑

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.1.61,新增新功能"
+git commit -a -m "发布1.1.62,修改呼叫逻辑"
 git push origin master
-git tag 1.1.61  
+git tag 1.1.62  
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
