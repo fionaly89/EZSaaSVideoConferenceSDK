0fa868b7f38dfa6ca6f9a835c8b99ac79bb970e8
fionaly89 <liyue271181776@gmail.com>
2021-08-30 19:15:08 +0800

    1.1.72版本，新增图标

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.1.68,新增联席主持人功能"
+git commit -a -m "发布1.1.72,更新图标"
 git push origin master
-git tag 1.1.68  
+git tag 1.1.72  
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
