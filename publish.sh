638ad53f74c96834d2c3307d87be7837298164e1
fionaly89 <liyue271181776@gmail.com>
2022-01-06 12:44:06 +0800

    去除多余日志 1.1.77

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.1.76,调整鉴权逻辑"
+git commit -a -m "发布1.1.77,去除多余的日志"
 git push origin master
-git tag 1.1.76  
+git tag 1.1.77  
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
