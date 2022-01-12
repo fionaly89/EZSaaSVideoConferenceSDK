d6e1048fd9dc2a64b6058ad59f8f3d45b014d2f1
fionaly89 <liyue271181776@gmail.com>
2022-01-12 14:33:03 +0800

    解决编译冲突问题 1.1.81

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.1.79,已知功能优化，已知问题修复"
+git commit -a -m "发布1.1.81,修复编译问题"
 git push origin master
-git tag 1.1.79  
+git tag 1.1.81  
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
