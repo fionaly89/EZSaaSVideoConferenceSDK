faabe8d643bca2fcfec2df2935958ba0b5b9d532
fionaly89 <liyue271181776@gmail.com>
2022-02-22 15:42:28 +0800

    调整呼叫组件初始化逻辑 1.1.84

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.1.83,调整静音逻辑"
+git commit -a -m "发布1.1.84,调整呼叫初始化逻辑"
 git push origin master
-git tag 1.1.83  
+git tag 1.1.84  
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
