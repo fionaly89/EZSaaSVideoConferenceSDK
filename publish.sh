fed8a168173b16ec449b519c52f62c6b03db88f7
fionaly89 <liyue271181776@gmail.com>
2022-02-23 11:39:44 +0800

    解决退出会议后显示未接听问题 1.1.85

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.1.84,调整呼叫初始化逻辑"
+git commit -a -m "发布1.1.85,解决退出会议后显示未接听问题"
 git push origin master
-git tag 1.1.84  
+git tag 1.1.85  
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
