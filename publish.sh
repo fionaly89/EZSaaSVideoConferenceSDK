4d24d1e62f38da92b1ba81d9792844a234a8be8d
fionaly89 <liyue271181776@gmail.com>
2022-02-28 16:29:35 +0800

    发布1.1.86,解决第一次入会失败的问题

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.1.85,解决退出会议后显示未接听问题"
+git commit -a -m "发布1.1.86,解决第一次入会失败的问题"
 git push origin master
-git tag 1.1.85  
+git tag 1.1.86  
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
