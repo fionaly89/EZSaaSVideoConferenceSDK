d759c9403d2d915b327175617a22d804f2823a72
fionaly89 <liyue271181776@gmail.com>
2022-04-20 16:13:41 +0800

    发布1.1.96,新增单聊、群聊呼叫功能

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.1.93,新增切换服务地址功能"
+git commit -a -m "发布1.1.96,新增呼叫功能"
 git push origin master
-git tag 1.1.93  
+git tag 1.1.96  
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
