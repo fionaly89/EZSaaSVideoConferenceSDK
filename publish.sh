322a42a5aef1e8d22dc58e80f7bd5a6749470d9f
fionaly89 <liyue271181776@gmail.com>
2022-04-21 20:47:45 +0800

    发布1.1.97,解决加入会议页面缺陷

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.1.96,新增呼叫功能"
+git commit -a -m "发布1.1.97,解决加入会议页面缺陷"
 git push origin master
-git tag 1.1.96  
+git tag 1.1.97  
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
