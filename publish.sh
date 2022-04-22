0dfa4fa82007085e77b30de3f258c9c963d30d94
fionaly89 <liyue271181776@gmail.com>
2022-04-22 14:14:48 +0800

    发布1.1.98,修改即时会议页面视频按钮被压缩问题,修改企业成员列表页面图标不显示问题

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.1.97,解决加入会议页面缺陷"
+git commit -a -m "发布1.1.98,解决企业成员列表页面图标不显示问题"
 git push origin master
-git tag 1.1.97  
+git tag 1.1.98  
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
