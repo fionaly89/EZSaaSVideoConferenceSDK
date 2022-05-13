24d579b281b83931d7efe8cef5fad7f18c2629d4
fionaly89 <liyue271181776@gmail.com>
2022-05-13 11:30:13 +0800

    发布1.2.3,解决呼叫接听后被自动取消等问题

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.2.2,解决无法挂断问题"
+git commit -a -m "发布1.2.3,解决呼叫接听后被自动取消等问题"
 git push origin master
-git tag 1.2.2  
+git tag 1.2.3  
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
