3f18338b6df6b450808f40230b16e03301b6b272
fionaly89 <liyue271181776@gmail.com>
2022-05-26 19:31:47 +0800

    发布1.2.7,修改多流模式下，共享屏幕画中画没有禁用问题；修改呼叫超时页面没有第一时间关闭问题

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.2.6,修改共享屏幕关闭摄像头问题，调整被叫页面视觉逻辑"
+git commit -a -m "发布1.2.7,修改多流模式下，共享屏幕画中画没有禁用问题；修改呼叫超时页面没有第一时间关闭问题"
 git push origin master
-git tag 1.2.6  
+git tag 1.2.7  
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
