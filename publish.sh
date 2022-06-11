e52d2bc69de867cebf83ffbfb846a392bc76ad52
fionaly89 <liyue271181776@gmail.com>
2022-06-11 17:43:58 +0800

    发布1.3.1,修改无法呼叫大量成员问题

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.3.0,修改呼叫功能问题"
+git commit -a -m "发布1.3.1,修改无法呼叫大量成员问题"
 git push origin master
-git tag 1.3.0 
+git tag 1.3.1 
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
