356240fd165837943116e8511182236127077f27
fionaly89 <liyue271181776@gmail.com>
2022-06-21 16:26:17 +0800

    发布1.3.7,修改内测问题-hik

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.3.6,修改内测问题-show"
+git commit -a -m "发布1.3.7,修改内测问题-hik"
 git push origin master
-git tag 1.3.6 
+git tag 1.3.7 
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
