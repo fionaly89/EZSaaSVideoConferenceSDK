b918f89fa4b9eeb3dffc43b1e01784559485b22d
fionaly89 <liyue271181776@gmail.com>
2022-06-23 16:13:08 +0800

    发布1.3.8,移除无用权限-show

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.3.7,修改内测问题-hik"
+git commit -a -m "发布1.3.8,移除无用权限-show"
 git push origin master
-git tag 1.3.7 
+git tag 1.3.8 
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
