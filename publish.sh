033ba4d90dffa958dc68f5e6b65265fe94cce4c3
fionaly89 <liyue271181776@gmail.com>
2022-06-27 11:24:01 +0800

    发布1.4.0,修改呼叫会议默认时长问题-hik

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.3.9,修改内测问题-hik"
+git commit -a -m "发布1.4.0,修改呼叫会议默认时长问题-hik"
 git push origin master
-git tag 1.3.9 
+git tag 1.4.0 
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
