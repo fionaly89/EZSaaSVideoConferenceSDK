d0d0c5b743b8d0ab5bf10a3084e7160c6c739b30
fionaly89 <liyue271181776@gmail.com>
2022-06-28 14:24:45 +0800

    发布1.4.1,修改退会取消呼叫问题问题-hik

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.4.0,修改呼叫会议默认时长问题-hik"
+git commit -a -m "发布1.4.1,修改退会取消呼叫问题问题-hik"
 git push origin master
-git tag 1.4.0 
+git tag 1.4.1 
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
