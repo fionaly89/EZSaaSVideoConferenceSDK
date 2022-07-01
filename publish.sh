93c596550dfab050ada366476344c18dd63515a7
fionaly89 <liyue271181776@gmail.com>
2022-07-01 18:02:11 +0800

    发布1.4.2,修改举手状态不正确问题-show

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.4.1,修改退会取消呼叫问题问题-hik"
+git commit -a -m "发布1.4.2,修改举手状态不正确问题-show"
 git push origin master
-git tag 1.4.1 
+git tag 1.4.2 
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
