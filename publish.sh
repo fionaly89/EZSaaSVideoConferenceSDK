39e63a91ee9cb89cd3a1e3844a5c5d805cbb28ff
fionaly89 <liyue271181776@gmail.com>
2022-07-05 20:49:15 +0800

    发布1.4.3,解决数组遍历可能导致的崩溃问题-show

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.4.2,修改举手状态不正确问题-show"
+git commit -a -m "发布1.4.3,解决数组遍历可能导致的崩溃问题-show"
 git push origin master
-git tag 1.4.2 
+git tag 1.4.3 
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
