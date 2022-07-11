433c3dd2741ff005e170667b8e9c4522435912d4
fionaly89 <liyue271181776@gmail.com>
2022-07-11 16:05:29 +0800

    发布1.4.5,解决多次查询异常问题-hik

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.4.4,新增查询呼叫状态功能-hik"
+git commit -a -m "发布1.4.5,解决多次查询异常问题-hik"
 git push origin master
-git tag 1.4.4 
+git tag 1.4.5 
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
