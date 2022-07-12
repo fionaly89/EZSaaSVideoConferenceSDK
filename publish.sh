9488a896bdd89560e5fad7f584d039d87a3e38d9
fionaly89 <liyue271181776@gmail.com>
2022-07-12 16:22:08 +0800

    发布1.4.7,解决错误的发送忙状态问题-hik

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.4.6,解决无法刷新hiklink头像问题-hik"
+git commit -a -m "发布1.4.7,解决错误的发送忙状态问题-hik"
 git push origin master
-git tag 1.4.6 
+git tag 1.4.7 
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
