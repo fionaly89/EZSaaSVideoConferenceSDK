cdb06eac102ac5b2c96cefb261376482b9a319b6
fionaly89 <liyue271181776@gmail.com>
2022-07-12 15:40:26 +0800

    发布1.4.6,解决无法刷新hiklink头像问题-hik

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.4.5,解决多次查询异常问题-hik"
+git commit -a -m "发布1.4.6,解决无法刷新hiklink头像问题-hik"
 git push origin master
-git tag 1.4.5 
+git tag 1.4.6 
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
