0a9a6d9b7f57541069b28061e5ddf19eb54a7a7f
fionaly89 <liyue271181776@gmail.com>
2022-08-02 16:45:28 +0800

    发布1.5.0,解决内测问题-hik

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.4.9,解决内测问题-hik"
+git commit -a -m "发布1.5.0,解决内测问题-hik"
 git push origin master
-git tag 1.4.9 
+git tag 1.5.0
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
