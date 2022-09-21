fde24e3f79a8bdd3a9a769e518e5fcdcdc98a712
fionaly89 <liyue271181776@gmail.com>
2022-09-21 09:14:43 +0800

    发布1.5.3,使用旧版本编译器重新打包-hik

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.5.2,解决编译冲突-hik"
+git commit -a -m "发布1.5.3,使用旧版本编译器重新打包-hik"
 git push origin master
-git tag 1.5.2
+git tag 1.5.3
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
