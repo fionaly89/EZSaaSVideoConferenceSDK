df3ff8fe30c60b0b67870c11b789adcaa7a728f3
fionaly89 <liyue271181776@gmail.com>
2022-10-19 16:13:17 +0800

    发布1.5.6,调整错误信息展示逻辑-hik

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.5.5,调整查询被叫接口，整合某些接口入参-hik"
+git commit -a -m "发布1.5.6,调整错误信息展示逻辑-hik"
 git push origin master
-git tag 1.5.5
+git tag 1.5.6
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
