6b68e462494dcc5a253c622f8bdae50fee64d114
fionaly89 <liyue271181776@gmail.com>
2022-11-15 20:49:56 +0800

    发布1.6.1，新增会中英文名功能、解决自测问题-hik

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.6.0，新增会中英文名功能、解决自测问题-hik"
+git commit -a -m "发布1.6.1，新增会中英文名功能、解决自测问题-hik"
 git push origin master
-git tag 1.6.0
+git tag 1.6.1
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
