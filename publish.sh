a498736ffdc2a02b78f3a862cb8590aefbc9a57c
fionaly89 <liyue271181776@gmail.com>
2022-11-29 11:31:06 +0800

    发布1.6.5，解决xcode13编译问题-hik

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.6.4，解决自测问题-hik"
+git commit -a -m "发布1.6.5，解决xcode13编译问题-hik"
 git push origin master
-git tag 1.6.4
+git tag 1.6.5
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
