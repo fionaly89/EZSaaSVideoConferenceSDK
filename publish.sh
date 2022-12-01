e811791c76ac2fcbf4ea24874ba3f1c8f7145b5e
fionaly89 <liyue271181776@gmail.com>
2022-12-01 16:26:07 +0800

    发布1.6.6，解决小窗口问题-hik

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.6.5，解决xcode13编译问题-hik"
+git commit -a -m "发布1.6.6，解决小窗口问题-hik"
 git push origin master
-git tag 1.6.5
+git tag 1.6.6
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
