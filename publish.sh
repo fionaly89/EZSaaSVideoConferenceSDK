dde8f557f7a561fbdc89fbe82ef653c31ebb88be
fionaly89 <liyue271181776@gmail.com>
2022-12-05 16:18:42 +0800

    发布1.6.7，解决内测问题-hik

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.6.6，解决小窗口问题-hik"
+git commit -a -m "发布1.6.7，解决内测问题-hik"
 git push origin master
-git tag 1.6.6
+git tag 1.6.7
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
