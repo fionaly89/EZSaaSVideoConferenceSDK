e928549dfe145e03f2fa74d2334351bd33ef58ce
fionaly89 <liyue271181776@gmail.com>
2023-02-09 15:02:24 +0800

    发布1.7.0，新增日志上报功能-hik

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.7.0，新增日志上报功能-hik"
+git commit -a -m "发布1.7.1，完善日志上报功能-hik"
 git push origin master
-git tag 1.7.0
+git tag 1.7.1
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
