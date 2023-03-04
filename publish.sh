bc2cfa36de61ad01d21a8c072c0d602d05b7bedd
fionaly89 <liyue271181776@gmail.com>
2023-03-04 17:03:48 +0800

    发布1.7.4，新增pip支持-hik

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.7.3，新增pip支持-hik"
+git commit -a -m "发布1.7.4，新增pip支持-hik"
 git push origin master
-git tag 1.7.3
+git tag 1.7.4
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
