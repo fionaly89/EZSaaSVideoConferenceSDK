24835d08e244ac2bc690e81696f4b503d90fb73c
fionaly89 <liyue271181776@gmail.com>
2023-03-08 19:56:46 +0800

    发布1.7.5，修改pip问题-hik

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.7.4，新增pip支持-hik"
+git commit -a -m "发布1.7.5，修改pip问题-hik"
 git push origin master
-git tag 1.7.4
+git tag 1.7.5
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
