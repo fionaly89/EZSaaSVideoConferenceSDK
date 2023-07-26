38431eb02a433e6efb2c551c75c2569af3c2417d
fionaly89 <liyue271181776@gmail.com>
2023-07-26 10:03:21 +0800

    发布1.9.3，线路自动选择功能(外网线路优先)-hik

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.9.2，完善线路自动选择功能-hik"
+git commit -a -m "发布1.9.3，线路自动选择功能(外网线路优先)-hik"
 git push origin master
-git tag 1.9.2
+git tag 1.9.3
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
