2a36bbb9a7ab8fe5064d70df89cd46a0b8772e00
fionaly89 <liyue271181776@gmail.com>
2023-08-17 15:29:23 +0800

    发布1.9.5，修改内测问题-ezviz

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布1.9.4，调整拷贝分享链接逻辑-ezviz"
+git commit -a -m "发布1.9.5，修改内测问题-ezviz"
 git push origin master
-git tag 1.9.4
+git tag 1.9.5
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
