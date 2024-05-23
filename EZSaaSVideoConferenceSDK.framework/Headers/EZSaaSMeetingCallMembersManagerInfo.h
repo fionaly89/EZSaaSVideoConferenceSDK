5a7864339b6a1c3572c40772df6f4ba70f670565
fionaly89 <liyue271181776@gmail.com>
2024-05-23 19:52:41 +0800

    发布HIKYSRTCCall.6，完善呼叫功能

--- EZSaaSVideoConferenceSDK.framework/Headers/EZSaaSMeetingCallMembersManagerInfo.h
+++ EZSaaSVideoConferenceSDK.framework/Headers/EZSaaSMeetingCallMembersManagerInfo.h
@@ -37,6 +37,15 @@ typedef NS_ENUM(NSInteger, EZSaaSInviteStatus) {
     
     /** 已邀请 */
     EZSaaSInviteStatus_Invite = 8,
+    
+    /** 通话中断 */
+    EZSaaSInviteStatus_Interrupt = 9,
+
+    /** 挂断 */
+    EZSaaSInviteStatus_Hangup = 10,
+    
+    /** 网络异常 */
+    EZSaaSInviteStatus_Abnormal = 11,
 };
 
 @interface EZSaaSMeetingCallMembersManagerInfo : NSObject
--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -a -m "发布2.0.3，新增研讨会功能-hik"
+git commit -a -m "发布2.0.5，新增呼叫功能-hik"
 git push origin master
-git tag 2.0.3
+git tag 2.0.5
 git push --tag
 pod trunk push EZSaaSVideoConferenceSDK.podspec --skip-import-validation --allow-warnings
